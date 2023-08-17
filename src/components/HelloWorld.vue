775d96cd7004f709dcd7e99e4e2d9fea85b53602
OXAYR <uzairmaqsood451@gmail.com>
2023-08-17 12:47:04 +0500

    Li working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,30 +7,30 @@
       Learn from the experts on the cutting-edge of deception at the most
       sinister companies.
     </p>
-    <div class="mt-24">
-      <div class="flex">
-        <div class="flex-col gap-x-32 flex-wrap text-left">
-          <li class="m-4">
-            <span class="text-sm text-blue-600">Opening Day </span>
-            <p class="text-2xl font-medium px-5 py-2 text-blue-950">April 4</p>
-          </li>
-          <li class="m-4">
-            <span class="text-sm text-gray-600">Speakers & Workshop </span>
-            <p class="text-2xl font-medium px-5 py-2 text-blue-950">April 5</p>
-          </li>
-          <li class="m-4">
-            <span class="text-sm text-gray-600">Interview</span>
-            <p class="text-2xl font-medium px-5 py-2 text-blue-950">April 6</p>
-          </li>
-        </div>
+    <div class="mt-24 flex flex-col-2">
+      <div class="flex-col text-left w-2/4">
+        <li>
+          <span class="text-sm text-blue-600">Opening Day </span>
+          <p class="text-2xl font-medium py-2 text-blue-950">April 4</p>
+        </li>
+        <li>
+          <span class="text-sm text-gray-600">Speakers & Workshop </span>
+          <p class="text-2xl font-medium px-5 py-2 text-blue-950">April 5</p>
+        </li>
+        <li>
+          <span class="text-sm text-gray-600">Interview</span>
+          <p class="text-2xl font-medium px-5 py-2 text-blue-950">April 6</p>
+        </li>
       </div>
       <div class="grid grid-cols-3 list-none">
         <li
           v-for="x in 9"
           :key="x"
-          class="p-4 bg-white shadow-md rounded-lg flex flex-col justify-between"
+          class="p-4 bg-white card rounded-lg flex flex-col m-4 justify-between"
         >
-          <div><img src="" alt="productimage" /></div>
+          <div>
+            <img src="../assets/steven-mchail.png" alt="productimage" />
+          </div>
           <div class="p-2">
             <h2 class="text-lg font-semibold">Steven McHail</h2>
             <p class="text-gray-500 text-sm line-clamp-2">
